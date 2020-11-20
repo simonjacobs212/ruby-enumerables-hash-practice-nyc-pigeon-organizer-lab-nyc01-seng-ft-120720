eba4b6167a05d6baf83bbd8acbf889e9a0bcd365
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-11-20 18:48:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,17 +1,18 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  final_results = data.each_with_object({}) do |(key, value), final_hash|
+  data.each_with_object({}) do |(key, value), final_hash|
     value.each do |inner_key, names|
       names.each do |name|
+        binding.pry
         if !final_hash[name]
           final_hash[name] = {}
         end
         if !final_hash[name][key]
-          final_hash[name][key] = final_hash[name][key].push(inner_key.to_s)
+          final_hash[name][key] = []
         end
+        final_hash[name][key].push(inner_key.to_s)
       end
     end
   end
-  final_results
 end
